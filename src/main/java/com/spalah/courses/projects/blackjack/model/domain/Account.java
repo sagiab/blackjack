b647d1259364317bc9577b1e0d85576a46bbe2fd
denisko21 <denisko21@yandex.ru>
2016-06-05 21:48:12 +0300

    some object fixs

--- src/main/java/com/spalah/courses/projects/blackjack/model/domain/Account.java
+++ src/main/java/com/spalah/courses/projects/blackjack/model/domain/Account.java
@@ -4,4 +4,43 @@ package com.spalah.courses.projects.blackjack.model.domain;
  * @author Denis Loshkarev on 03.06.2016.
  */
 public class Account {
+    private Long id;
+    private String login;
+    private String nickName;
+    private Long balance;
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public Long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Long balance) {
+        this.balance = balance;
+    }
 }
--- src/main/java/com/spalah/courses/projects/blackjack/model/domain/commands/CommandBet.java
+++ src/main/java/com/spalah/courses/projects/blackjack/model/domain/commands/CommandBet.java
@@ -4,4 +4,17 @@ package com.spalah.courses.projects.blackjack.model.domain.commands;
  * @author Denis Loshkarev on 03.06.2016.
  */
 public class CommandBet extends Command {
+    private Long sum;
+
+    public CommandBet() {
+        super();
+    }
+
+    public Long getSum() {
+        return sum;
+    }
+
+    public void setSum(Long sum) {
+        this.sum = sum;
+    }
 }
