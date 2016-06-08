261eabb6312f71c9dae2e6bf1a1cb42c01dedf07
cyxouD <cyxoud@mail.ru>
2016-06-09 01:16:44 +0300

    added mapping

---
+++ src/main/java/com/spalah/courses/projects/blackjack/model/domain/communication/TablePlayerCommunication.java
@@ -0,0 +1,7 @@
+package com.spalah.courses.projects.blackjack.model.domain.communication;
+
+/**
+ * Created by Dima on 08.06.2016.
+ */
+public class TablePlayerCommunication {
+}
---
+++ src/main/java/com/spalah/courses/projects/blackjack/model/domain/table/TableGame.java
@@ -0,0 +1,7 @@
+package com.spalah.courses.projects.blackjack.model.domain.table;
+
+/**
+ * Created by Dima on 08.06.2016.
+ */
+public class TableGame {
+}
