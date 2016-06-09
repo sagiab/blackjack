fcc83bdfe36fb8848adb0251ba9e6608c366f843
denisko21 <denisko21@yandex.ru>
2016-06-09 15:36:25 +0300

    Added URL mapping

--- src/main/java/com/spalah/courses/projects/blackjack/controller/TestController.java
+++ src/main/java/com/spalah/courses/projects/blackjack/controller/TestController.java
@@ -2,7 +2,11 @@ package com.spalah.courses.projects.blackjack.controller;
 
 import com.spalah.courses.projects.blackjack.model.service.TableService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Denis Loshkarev.
@@ -12,6 +16,8 @@ public class TestController {
     @Autowired
     TableService tableService;
 
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
     public void test() {
 //        tableService.getTableTypesVariants();
     }
